2c33033bd8956ac3910ed8737fba64c0b1fa3a3d
Gareth <grogers@thoughtworks.com>
2013-04-25 12:30:21 +0100

    Cut new library version, no longer using Sizzle

--- release_templates/stagepromptTemplate.js
+++ release_templates/stagepromptTemplate.js
@@ -1,5 +1,4 @@
-// @depend ../script/ext/sizzle.js
-// @depend ../script/patch.js
 // @depend ../script/namespace.js
+// @depend ../script/patch.js
 // @depend ../script/cookieUtils.js
 // @depend ../script/stageprompt.js
