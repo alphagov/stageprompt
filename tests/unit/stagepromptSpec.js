9818cbf4d7e061803acdaf9f91c1f5b86e59409f
Max Fliri <mfliri@thoughtworks.com>
2013-05-29 15:08:00 +0100

    handle attributes on body descendants
    @gtrogers
    @maxfliri

--- tests/unit/stagepromptSpec.js
+++ tests/unit/stagepromptSpec.js
@@ -1,58 +1,60 @@
 describe("stageprompt", function () {
-  
+
   afterEach(function () {
     document.getElementById('sandbox').innerHTML = "";
   });
-  
+
   it("should exist... probably", function () {
     expect(GOVUK.performance.stageprompt).not.toBeNull();
   });
-  
+
   it("should not blow up if there are no data-journey tags", function () {
     expect(Sizzle("[data-journey]").length).toBe(0);
     GOVUK.performance.stageprompt.setup({
-      analyticsFunction: function () {}
+      analyticsFunction:function () {
+      }
     });
     // should not blow up
   });
-  
+
   describe("journey events from landing on a page", function () {
-    
+
     afterEach(function () {
       document.getElementsByTagName('body')[0].removeAttribute('data-journey');
       document.getElementById('sandbox').removeAttribute('data-journey');
     });
-    
+
     it("should send an event if the page has a journey-data tag on the body", function () {
-      document.getElementsByTagName('body')[0].setAttribute('data-journey','test-journey:someStage');
+      document.getElementsByTagName('body')[0].setAttribute('data-journey', 'test-journey:someStage');
       var spy = jasmine.createSpy();
       GOVUK.performance.stageprompt.setup({
-        analyticsFunction: spy
+        analyticsFunction:spy
       });
-      
+
       expect(spy).toHaveBeenCalledWith("test-journey:someStage");
     });
-    
-    it("should not send an event if the data-journey tag is not on the body", function () {
-      document.getElementById('sandbox').setAttribute('data-journey','test-journey:no-sendy');
-      
+
+    it("should send an event if the page has a journey-data tag on another tag", function () {
+      document.getElementById('sandbox').setAttribute('data-journey', 'test-journey:nextStep');
+
       var spy = jasmine.createSpy();
       GOVUK.performance.stageprompt.setup({
-        analyticsFunction: spy
+        analyticsFunction:spy
       });
-      
-      expect(spy).not.toHaveBeenCalled();
-    })
-    
-    it("should NOT send an event if the journey-data tag was on the link", function () {
-      document.getElementById("sandbox").innerHTML += "<a id='link' data-journey='test-journey:start'>Link</a>";      
-      
-      var spy = jasmine.createSpy();
-      GOVUK.performance.stageprompt.setup({
-        analyticsFunction: spy
-      });
-      
-      expect(spy).not.toHaveBeenCalled();
+
+      expect(spy).toHaveBeenCalledWith("test-journey:nextStep");
     });
+
+    it("should send one event if the page has multiple elements with data-journey attribute", function () {
+      document.getElementById('sandbox').setAttribute('data-journey', 'test-journey:stuff');
+      document.getElementById('sandbox').innerHTML = "<p id='foo' data-journey='test-journey:moreStuff'>something</p>";
+      var spy = jasmine.createSpy();
+      GOVUK.performance.stageprompt.setup({
+        analyticsFunction:spy
+      });
+
+      expect(spy.callCount).toBe(1);
+    });
+
   })
 });
