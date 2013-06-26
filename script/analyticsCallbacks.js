865242280c99c4a5b2084ba53cd2e998caceed64
Gareth <grogers@thoughtworks.com>
2013-06-26 16:56:45 +0100

    argument for gaq should be an array

--- script/analyticsCallbacks.js
+++ script/analyticsCallbacks.js
@@ -3,5 +3,5 @@ var GOVUK = GOVUK || {};
 GOVUK.performance = GOVUK.performance || {};
 
 GOVUK.performance.sendGoogleAnalyticsEvent = function (action) {
-  _gaq.push('_trackEvent', 'stagePrompt', action, undefined, undefined, true);
+  _gaq.push(['_trackEvent', 'stagePrompt', action, undefined, undefined, true]);
 };
--- spec/javascripts/analyticsCallbacksSpec.js
+++ spec/javascripts/analyticsCallbacksSpec.js
@@ -10,17 +10,17 @@ describe("Google Analytics callback", function () {
     GOVUK.performance.sendGoogleAnalyticsEvent('test');
     
     expect(_gaq.push).toHaveBeenCalled();
-    expect(_gaq.push.argsForCall[0][0]).toEqual('_trackEvent');
+    expect(_gaq.push.argsForCall[0][0][0]).toEqual('_trackEvent');
   });
   
   it("should use sensible default values... eg. non interaction events so as not to mess with bounce rate", function () {
     GOVUK.performance.sendGoogleAnalyticsEvent('test event')
     
-    var category = _gaq.push.argsForCall[0][1],
-        action = _gaq.push.argsForCall[0][2],
-        label = _gaq.push.argsForCall[0][3],
-        value = _gaq.push.argsForCall[0][4],
-        nonInteraction = _gaq.push.argsForCall[0][5];
+    var category = _gaq.push.argsForCall[0][0][1],
+        action = _gaq.push.argsForCall[0][0][2],
+        label = _gaq.push.argsForCall[0][0][3],
+        value = _gaq.push.argsForCall[0][0][4],
+        nonInteraction = _gaq.push.argsForCall[0][0][5];
     
     expect(category).toEqual('stagePrompt');
     expect(action).toEqual('test event');
