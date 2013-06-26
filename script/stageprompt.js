ec7b14f3b8f6976b544b0b853e743927c76f07d8
Gareth <grogers@thoughtworks.com>
2013-06-26 12:05:39 +0100

    Send an analytics event for clicks on stuff with data-journey-helper

--- script/stageprompt.js
+++ script/stageprompt.js
@@ -9,9 +9,16 @@ GOVUK.performance.stageprompt = (function () {
   var setup;
 
   setup = function (analyticsCallback) {
-    var journeyStage = $('[data-journey]').attr('data-journey');
+    var journeyStage = $('[data-journey]').attr('data-journey'),
+        journeyHelpers = $('[data-journey-helper]');
     if (journeyStage) {
       analyticsCallback(journeyStage);
+    }
+    
+    if (journeyHelpers.length) {
+      journeyHelpers.on('click', function (event) {
+        analyticsCallback($(this).data('journey-helper'));
+      });
     }
   };
 
