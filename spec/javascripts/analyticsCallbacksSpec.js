260a2d4edb242393e06456858f6c57534565a246
Gareth <grogers@thoughtworks.com>
2013-06-26 15:35:46 +0100

    Add wrapper for easily sending stuff to google

---
+++ spec/javascripts/analyticsCallbacksSpec.js
@@ -0,0 +1,31 @@
+describe("Google Analytics callback", function () {
+  
+  _gaq = [];
+  
+  beforeEach(function () {
+    spyOn(_gaq, 'push');
+  });
+  
+  it("should push an event onto the google analytics que", function () {
+    GOVUK.performance.sendGoogleAnalyticsEvent('test');
+    
+    expect(_gaq.push).toHaveBeenCalled();
+    expect(_gaq.push.argsForCall[0][0]).toEqual('_trackEvent');
+  });
+  
+  it("should use sensible default values... eg. non interaction events so as not to mess with bounce rate", function () {
+    GOVUK.performance.sendGoogleAnalyticsEvent('test event')
+    
+    var category = _gaq.push.argsForCall[0][1],
+        action = _gaq.push.argsForCall[0][2],
+        label = _gaq.push.argsForCall[0][3],
+        value = _gaq.push.argsForCall[0][4],
+        nonInteraction = _gaq.push.argsForCall[0][5];
+    
+    expect(category).toEqual('stagePrompt');
+    expect(action).toEqual('test event');
+    expect(label).toEqual(null);
+    expect(value).toEqual(null);
+    expect(nonInteraction).toEqual(true);
+  })
+});
